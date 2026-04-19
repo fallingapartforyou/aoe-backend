49f9a6cd92b39476c46b8e0ba018161e7cda69bd
Huy Nguyen <watersondarwin747@gmail.com>
2026-04-19 13:36:19 +0700

    add migration init

---
+++ Migrations/20260419063534_Init.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace aoe.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "assignments",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    question_type = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    question_count = table.Column<int>(type: "integer", nullable: false),
+                    open_time = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    close_time = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    show_result = table.Column<bool>(type: "boolean", nullable: true, defaultValue: true),
+                    show_explanation = table.Column<bool>(type: "boolean", nullable: true, defaultValue: true),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("assignments_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "results",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    student_id = table.Column<int>(type: "integer", nullable: false),
+                    assignment_id = table.Column<int>(type: "integer", nullable: false),
+                    score = table.Column<int>(type: "integer", nullable: false),
+                    submitted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_results", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentAnswers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StudentId = table.Column<int>(type: "integer", nullable: false),
+                    AssignmentId = table.Column<int>(type: "integer", nullable: false),
+                    QuestionId = table.Column<int>(type: "integer", nullable: false),
+                    Answer = table.Column<string>(type: "text", nullable: true),
+                    IsCorrect = table.Column<bool>(type: "boolean", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentAnswers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    email = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    phone = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    password = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    role = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("users_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "questions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    assignment_id = table.Column<int>(type: "integer", nullable: false),
+                    type = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    content = table.Column<string>(type: "text", nullable: false),
+                    correct_answer = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    explanation = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("questions_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "questions_assignment_id_fkey",
+                        column: x => x.assignment_id,
+                        principalTable: "assignments",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "classes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    class_code = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: false),
+                    teacher_id = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("classes_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "classes_teacher_id_fkey",
+                        column: x => x.teacher_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "submissions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    assignment_id = table.Column<int>(type: "integer", nullable: false),
+                    student_id = table.Column<int>(type: "integer", nullable: false),
+                    start_time = table.Column<DateTime>(type: "timestamp without time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    submit_time = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    score = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("submissions_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "submissions_assignment_id_fkey",
+                        column: x => x.assignment_id,
+                        principalTable: "assignments",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "submissions_student_id_fkey",
+                        column: x => x.student_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "question_options",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    question_id = table.Column<int>(type: "integer", nullable: false),
+                    label = table.Column<char>(type: "character(1)", maxLength: 1, nullable: false),
+                    content = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("question_options_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "question_options_question_id_fkey",
+                        column: x => x.question_id,
+                        principalTable: "questions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "assignment_classes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    assignment_id = table.Column<int>(type: "integer", nullable: false),
+                    class_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("assignment_classes_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "assignment_classes_assignment_id_fkey",
+                        column: x => x.assignment_id,
+                        principalTable: "assignments",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "assignment_classes_class_id_fkey",
+                        column: x => x.class_id,
+                        principalTable: "classes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "class_students",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    class_id = table.Column<int>(type: "integer", nullable: false),
+                    student_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("class_students_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "class_students_class_id_fkey",
+                        column: x => x.class_id,
+                        principalTable: "classes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "class_students_student_id_fkey",
+                        column: x => x.student_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "answers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    submission_id = table.Column<int>(type: "integer", nullable: false),
+                    question_id = table.Column<int>(type: "integer", nullable: false),
+                    answer = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    is_correct = table.Column<bool>(type: "boolean", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("answers_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "answers_question_id_fkey",
+                        column: x => x.question_id,
+                        principalTable: "questions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "answers_submission_id_fkey",
+                        column: x => x.submission_id,
+                        principalTable: "submissions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_answers_question_id",
+                table: "answers",
+                column: "question_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_answers_submission_id",
+                table: "answers",
+                column: "submission_id");
+
+            migrationBuilder.CreateIndex(
+                name: "assignment_classes_assignment_id_class_id_key",
+                table: "assignment_classes",
+                columns: new[] { "assignment_id", "class_id" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "idx_assignment_classes_assignment",
+                table: "assignment_classes",
+                column: "assignment_id");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_assignment_classes_class",
+                table: "assignment_classes",
+                column: "class_id");
+
+            migrationBuilder.CreateIndex(
+                name: "class_students_class_id_student_id_key",
+                table: "class_students",
+                columns: new[] { "class_id", "student_id" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_class_students_student_id",
+                table: "class_students",
+                column: "student_id");
+
+            migrationBuilder.CreateIndex(
+                name: "classes_class_code_key",
+                table: "classes",
+                column: "class_code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "idx_classes_teacher",
+                table: "classes",
+                column: "teacher_id");
+
+            migrationBuilder.CreateIndex(
+                name: "question_options_question_id_label_key",
+                table: "question_options",
+                columns: new[] { "question_id", "label" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "idx_questions_assignment",
+                table: "questions",
+                column: "assignment_id");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_submissions_student",
+                table: "submissions",
+                column: "student_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_submissions_assignment_id",
+                table: "submissions",
+                column: "assignment_id");
+
+            migrationBuilder.CreateIndex(
+                name: "users_email_key",
+                table: "users",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "users_phone_key",
+                table: "users",
+                column: "phone",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "answers");
+
+            migrationBuilder.DropTable(
+                name: "assignment_classes");
+
+            migrationBuilder.DropTable(
+                name: "class_students");
+
+            migrationBuilder.DropTable(
+                name: "question_options");
+
+            migrationBuilder.DropTable(
+                name: "results");
+
+            migrationBuilder.DropTable(
+                name: "StudentAnswers");
+
+            migrationBuilder.DropTable(
+                name: "submissions");
+
+            migrationBuilder.DropTable(
+                name: "classes");
+
+            migrationBuilder.DropTable(
+                name: "questions");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "assignments");
+        }
+    }
+}
