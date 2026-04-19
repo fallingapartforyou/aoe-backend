c2d13b28d1a2850813496a128aa4f2e3710f1805
Huy Nguyen <watersondarwin747@gmail.com>
2026-04-19 10:41:16 +0700

    update

--- Program.cs
+++ Program.cs
@@ -60,8 +60,8 @@ builder.Services.AddCors(options =>
             policy
             .WithOrigins(
                 "http://127.0.0.1:5500",
-                "http://localhost:5500"
-            "https://aoe-frontend.onrender.com"
+                "http://localhost:5500",
+                "https://aoe-frontend.onrender.com"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
